bd146ccb0a9af709c01f6cd0784f2cae234f0016
cheyipai <cheyipai@cheyipaidemacbook-pro-2.local>
2016-06-02 14:16:50 +0800

    Initial Commit

---
+++ 生命周期的测试/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  生命周期的测试
+//
+//  Created by cheyipai on 16/6/2.
+//  Copyright © 2016年 cheyipai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ 生命周期的测试/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  生命周期的测试
+//
+//  Created by cheyipai on 16/6/2.
+//  Copyright © 2016年 cheyipai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
